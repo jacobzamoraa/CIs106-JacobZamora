b9f0bee5ad19da85a411d66e786292a163355d08
jacobzamoraa <70067366+jacobzamoraa@users.noreply.github.com>
2020-09-10 18:23:35 -0700

    Create Activity 3.py

---
+++ Assignment 3/Activity 3.py
@@ -0,0 +1,5 @@
+hours = float(input())
+rate = float(input())
+weeklyamount = hours * rate
+annualamount = hours * rate * 52 / 12
+print("Weekly amount = " + str(weeklyamount) + "Month amount = " + str(monthlyamount) + "Annual amount = " + str(annualamount))
