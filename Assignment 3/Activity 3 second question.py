5717fa350150a39543fdf63e68e2c5f0ec09d398
jacobzamoraa <70067366+jacobzamoraa@users.noreply.github.com>
2020-09-10 18:34:42 -0700

    Create Activity 3 second question.py

---
+++ Assignment 3/Activity 3 second question.py
@@ -0,0 +1,10 @@
+print("How old are you in years")
+yearsOld = int(input())
+monthsOld = yearsOld * 12
+daysOld = yearsOld * 365
+hoursOld = yearsOld * 365 * 24
+secondsOld = yearsOld * 365 * 24 * 3600
+print("You are" + str(monthsOld) + "months old")
+print("You are" + str(daysOld) + "days old")
+print("You are" + str(hoursOld) + "Hours old")
+print("You are" + str(secondsOld) + "seconds old")
