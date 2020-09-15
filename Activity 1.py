1a2cdf3819bf7516112271147c6dcf1dd91a209e
jacobzamoraa <70067366+jacobzamoraa@users.noreply.github.com>
2020-09-14 17:14:48 -0700

    Delete Activity 1.py

--- Activity 1.py
+++
@@ -1,8 +0,0 @@
-# This is a program asking the user for a distance in miles and converting the mile into yards, feet and inches
-eachmile = int(input())
-yards = 1760 * eachmile
-inches = 63360 * eachmile
-feet = 5280 * eachmile
-print(1760 * mile)
-print(63360 * mile)
-print(52880 * mile)
