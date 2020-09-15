ce54c89dc6f2d22d52b9ea98c6fdcf360c755549
jacobzamoraa <70067366+jacobzamoraa@users.noreply.github.com>
2020-09-14 17:10:15 -0700

    Create Activity 2.py

---
+++ assignment 4/Activity 2.py
@@ -0,0 +1,6 @@
+# This is a program asking the user how old their dog is and converting it to human years.
+dogYear = int(input())
+dogYear = humanYear
+amount = dogYear * humanYear
+amount = 7 * 14
+print(98)
