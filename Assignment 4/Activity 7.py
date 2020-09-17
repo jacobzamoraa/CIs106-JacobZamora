54ced0b981186b100b302e8bdc224e6ff8b0da1f
jacobzamoraa <70067366+jacobzamoraa@users.noreply.github.com>
2020-09-16 18:44:59 -0700

    Create Activity 7.py
    Changed file to Python

---
+++ Assignment 4/Activity 7.py
@@ -0,0 +1,6 @@
+# This is a program asking the user for their dogs name and asking for dogs age to covert to human years.
+dogsname = input()
+dogsage = int(input())
+age = 7 * dogsage
+print(7 * dogsage, end='', flush=True)
+print(dogsname)
