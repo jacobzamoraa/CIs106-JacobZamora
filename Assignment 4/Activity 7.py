697db2ae8eeea745d7d745a7a398f48ee659bec8
Dave Braunschweig <dbraunsc@harpercollege.edu>
2020-09-18 11:57:23 -0500

    Assignment 4 corrections

--- Assignment 4/Activity 7.py
+++ Assignment 4/Activity 7.py
@@ -1,6 +1,12 @@
-# This is a program asking the user for their dogs name and asking for dogs age to covert to human years.
-dogsname = input()
-dogsage = int(input())
-age = 7 * dogsage
-print(7 * dogsage, end='', flush=True)
-print(dogsname)
+# This is a program asking the user for their dogs name and asking for
+# a dogs age to covert to human years.
+
+print("Enter dog's name:")
+dogs_name = input()
+
+print("Enter dog's age:")
+dogs_age = int(input())
+
+age = 7 * dogs_age
+
+print(dogs_name + " is " + str(age) + " years old in dog years.")
