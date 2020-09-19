052d86a1c8c806c565e7b8b35d21fe535940b56e
Dave Braunschweig <dbraunsc@harpercollege.edu>
2020-09-18 20:32:16 -0500

    Assignment 4 corrections

--- Assignment 4/Activity 3.py
+++ Assignment 4/Activity 3.py
@@ -1,7 +1,11 @@
-d_miles = int(input("Input distance in miles: "))
-d_inches = d_miles * 63360
-d_feet = d_miles * 5280
-d_yards = d_miles * 1760
-print("The distance in inches is %i inches." %d_inches)
-print("The distance in feet is %.2f feet." % d_feet)
-print("The distance in yards is %.2f yards." % d_yards)
+# Comments go here ...
+
+miles = int(input("Input distance in miles: "))
+
+inches = miles * 63360
+feet = miles * 5280
+yards = miles * 1760
+
+print(f"The distance in inches is {inches} inches.")
+print(f"The distance in feet is {feet:.2f} feet.")
+print(f"The distance in yards is {yards:.2f} yards.")
