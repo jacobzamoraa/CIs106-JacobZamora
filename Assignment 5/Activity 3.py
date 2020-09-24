111fc996246a5642cb4709233651c7eefd284c6d
jacobzamoraa <70067366+jacobzamoraa@users.noreply.github.com>
2020-09-23 20:07:05 -0700

    Update Activity 3.py
    Updated

--- Assignment 5/Activity 3.py
+++ Assignment 5/Activity 3.py
@@ -1,15 +1,26 @@
 def cleanUp():
     print("Thank you for using this program.")
 
+def displayresults():
+    print("displayresults", end='', flush=True)
+
 def feet(miles):
     var_return = 5280 * miles
     
     return var_return
 
+def imputmiles():
+    print("type in the miles you aregoing to be using to convert to yards, inches, feet.")
+
 def inches(miles):
     var_return = 63360 * miles
     
     return var_return
+
+def miles(miles):
+    miles = int(input())
+    
+    return Return
 
 def yards(miles):
     var_return = 1760 * miles
@@ -18,13 +29,15 @@ def yards(miles):
 
 # Main
 # This is a program converting miles to yards, inches and feet.
-print("please enter miles to convert to yards,inches,feet")
+imputmiles()
 miles = int(input())
+yards = int(input())
 inches = int(input())
 feet = int(input())
 yards = yards(miles)
 inches = inches(miles)
 feet = feet(miles)
+displayresults()
 print(yards * miles)
 print(inches * miles)
 print(feet * miles)
