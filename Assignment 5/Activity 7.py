eb753388d284c1197d565f5f51f03d55d8aa9c91
jacobzamoraa <70067366+jacobzamoraa@users.noreply.github.com>
2020-09-23 20:07:56 -0700

    Update Activity 7.py
    Updated

--- Assignment 5/Activity 7.py
+++ Assignment 5/Activity 7.py
@@ -6,11 +6,19 @@ def age(dogsage):
 def cleanUp():
     print("thank you for using this program")
 
+def displayResults():
+    print("results are")
+
+def dogsName():
+    print("imput dogs name")
+
 # Main
 # This is a program asking the user for their dogs name and age, converting the age to human years.
+dogsName()
 dogsname = input()
 dogsage = int(input())
 age = age(dogsage)
+displayResults()
 print(dogsname)
 print(dogsage)
 cleanUp()
